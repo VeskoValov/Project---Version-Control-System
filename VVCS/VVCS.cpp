fb642b5785b98cd8a944384f00ed658a8876c8af
VeselinValov <72752458+VeskoValov@users.noreply.github.com>
2020-12-19 23:14:28 +0200

    OneFunction

--- VVCS/VVCS.cpp
+++ VVCS/VVCS.cpp
@@ -1,20 +1,71 @@
-// VVCS.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <string>
+#include <cstdlib>
+#include <fstream>
+#include <cstring>
+#include <direct.h>
+ 
+using namespace std;
+//void commands(string command, string name);
+int main() {
+	string fileName;
+	string toBeAddedNames[64];
+	int startPosition = 0;
+	int counter = 0;
+	int lenght = 0;
+	getline(cin, fileName);
+	for (int i = 0, j = 0; i < fileName.size(); i++) {
+		counter++;
+		if (fileName[i] == ' ' || fileName[i + 1] == '\0') {
+			toBeAddedNames[j++] = fileName.substr(startPosition, counter);
+			lenght += counter;
+			startPosition = lenght;
+			counter = 0;
+		}
+	}
+	for (string str : toBeAddedNames) { cout << str << endl; }
 
-int main()
-{
-    std::cout << "Hello World!\n";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//	string command;
+//	string name;
+//	string dir = "Directory_Of_Repositories";
+//	//fs::create_directory(dir);
+//	//fs::permissions(dir, fs::perms::owner_all | fs::perms::group_all, fs::perm_options::add);
+//	cin >> command;
+//	getline(cin, name);
+//	cout << command << " " << name << endl;
+//	commands(command, name);
+//}
+// 
+//void commands(string command, string name) {
+//	string path_str = "C:\\Users\\Vesko\\source\\Dir\\" + name;
+//	string path_file_str = path_str + "\\" + "log.txt";
+//	if (command == "init") {
+//		_mkdir(path_str.c_str());
+//		ofstream file(path_file_str.c_str());
+//		if (!file.is_open()) {
+//			std::cout << "File error!\n";
+//		}
+//		else {
+//			file.write("balabalbla", 10);
+//			file.close();
+//		}
+// 
+//	}
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+// file.open(".\bla-bla\test.txt",std::ios::out);
--- YVCS/YVCS.cpp
+++ YVCS/YVCS.cpp
@@ -1,33 +1,100 @@
 #include <iostream>
-#include<windows.h>
-#include<string>
+#include <windows.h>
+#include <string>
 #include <cstdlib>
 #include <filesystem>
 #include <fstream>
+#include <cstring>
+#include <direct.h>
+#include <time.h>
+#pragma warning(disable:4996);
 using namespace std;
 namespace fs = filesystem; // c++ 17
-void commands(string command, string name);
+void commands(string command, string name, string message, string toBeAddedNames[64]);
 int main() {
 	string command;
+	string message;
+	string fileName;
+	string toBeAddedNames[64];
 	string name;
-	string dir = "Directory_Of_Repositories";
-	/* system(("mkdir " + dir).c_str());
-	 system("cls");
-	 system("cd Directory_Of_Repositories");
-	 */
+	bool isEntered = false;
+	string dir = "C:\\Directory_Of_Repositories";
+	cout << "Your diractory is located in disk C:\\Directory_Of_Repositories" << endl;
 	fs::create_directory(dir);
-	cin >> command;
-	getline(cin, name);
-	cout << command << " " << name << endl;
-	commands(command, name);
+	do {
+		cout << "Create a new repo with 'init' or load an existing one with 'load'";
+		cin >> command;
+		if (command != "exit" && !isEntered) {
+			if (command == "init") {
+				cout << "A new repository is created successfully.";
+			}
+			do  {
+				cin.ignore();
+				getline(cin, name);
+				isEntered = true;
+				if (!(command == "load" || command == "init")) {
+					cout << "Enter a valid command! ";
+				}
+			} while (command != "load" || command != "init");
+		}
+		if (command == "commit") {
+			getline(cin, message);
+		}
+		if (command == "add") {
+			int startPosition = 0;
+			int counter = 0;
+			int lenght = 0;
+			getline(cin, fileName);
+			for (int i = 0, j = 0; i < fileName.size(); i++) {
+				counter++;
+				if (fileName[i] == ' ' || fileName[i + 1] == '\0') {
+					toBeAddedNames[j++] = fileName.substr(startPosition, counter);
+					lenght += counter;
+					startPosition = lenght;
+					counter = 0;
+				}
+			}
+		}
+		//cout << command << " " << name << endl;
+		commands(command, name, message, toBeAddedNames);
+	} while (command != "exit");
 }
-void commands(string command, string name) {
+void commands(string command, string name, string message, string toBeAddedNames[64]) {
+	string path_str = "C:\\Directory_Of_Repositories\\" + name;
+	string path_file_str = path_str + "\\" + "log.txt";
+	string path_version = "C:\\Directory_Of_Repositories\\" + name + "\\" + message;
+	ofstream file;
 	if (command == "init") {
-		fs::create_directory(".\\Directory_Of_Repositories\\" + name);
-		fstream file;
-		file.open(".\\Directory_Of_Repositories\\" + name + "\\log.txt", std::ios::out);
-		file.write("balabalbla", 10);
+		_mkdir(path_str.c_str());
+		file.open(path_file_str.c_str(), std::ios::out);
 		file.close();
 	}
+	else if (command == "commit") {
+		const int MAX_TIME_CHARACTERS = 64;
+		time_t rawtime;
+		tm* timeinfo;
+		char date[MAX_TIME_CHARACTERS];
+		time(&rawtime);
+		timeinfo = localtime(&rawtime);
+		strftime(date, MAX_TIME_CHARACTERS, "%d/%m/%Y | %R", timeinfo);
+		file.open(path_file_str.c_str(), std::ios::out | std::ios::app);
+		cout << name << endl;
+		cout << strerror(errno);
+		file << date << " | " << message << endl;
+		file.close();
+		_mkdir(path_version.c_str());
+	}
+	else if (command == "push") {
+		for (int i = 0; toBeAddedNames[i] != "\0"; i++) {
+		fs:move(path_version);
+		}
+	}
+	
+	/*else if (command == "add") {
+	
+	}*/
 }
+//$USER
+//std::ios::out | std::ios::append
+//std::ios::in
 // file.open(".\bla-bla\test.txt",std::ios::out);
